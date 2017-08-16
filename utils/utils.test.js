f355885482d82748727d2d74e050d56c6fd6e9e8
Jawann Carmona <jcarmona@ACTNY-IT-Jawann-Carmona.local>
2017-08-16 16:50:04 -0400

    quick intro testing mocha node-tests

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -4,12 +4,16 @@ const utils = require('./utils')
 
 it('should add two numbers', () => {
 	var res = utils.add(33, 11)
-	expect(res).toBe(44, ['Woooo'])
+	expect(res).toBe(44).toBeA('number')
+	// if (res != 44) {
+	// 	throw new Error(`Expected 44, but got ${res}`)
+	// }
 });
 
 it('should square a number', () => {
 	var res = utils.square(5)
-	if (res != 25) {
-		throw new Error(`Expected 25, but got ${res}`)
-	}
+	expect(res).toBe(25).toBeA('number')
+	// if (res != 25) {
+	// 	throw new Error(`Expected 25, but got ${res}`)
+	// }
 })
